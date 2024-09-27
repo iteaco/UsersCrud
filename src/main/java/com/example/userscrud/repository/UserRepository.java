17fbbbf3b77d9b32ef2aa4b6d7ed6e6742a24a33
unknown <nhansynergisticit3@gmail.com>
2024-09-26 22:49:14 -0500

    first commit

---
+++ src/main/java/com/example/userscrud/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.example.userscrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.userscrud.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query(value = "SELECT * FROM user WHERE email = ?1", nativeQuery = true)
+	User findByEmailAddress(String emailAddress);
+
+	@Query(value = "DELETE FROM user WHERE email = :email", nativeQuery = true)
+	User deleteByEmail(String email);
+	
+	//=============================
+	
+	@Query(value = "SELECT * FROM user WHERE name = ?1", nativeQuery = true)
+	User findByName(String name);
+	
+	@Query(value = "DELETE FROM user WHERE name = :name", nativeQuery = true)
+	User deleteByName(String name);
+}
---
+++ src/main/java/com/example/userscrud/service/PostServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.userscrud.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.userscrud.entity.Post;
+import com.example.userscrud.repository.PostRepository;
+import com.example.userscrud.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostServiceImpl implements PostService {
+	
+	private PostRepository postRepository;
+	
+	/*
+	public PostServiceImpl(PostRepository postRepository) {
+		this.postRepository=postRepository;
+	}
+	*/
+	
+	@Override
+	public Post createPost(Post post) {
+		return postRepository.save(post);
+	}
+
+}
